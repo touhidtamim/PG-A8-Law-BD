236c1eabff3e5b8210af3563d1bd674ce032fb2e
Touhid Tamim <touhidtamim1@gmail.com>
2025-04-22 20:19:18 +0600

    updated project name and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ function App() {
 
   return (
     <>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-<button className='btn btn-primary'>show more</button>
 
     </>
   )
