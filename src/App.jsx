1042036e2534b49819594fd5069d30b9269e186c
Touhid Tamim <touhidtamim1@gmail.com>
2025-04-22 15:20:40 +0600

    Set up React project with Vite, Tailwind CSS, and Daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+<button className='btn btn-primary'>show more</button>
+
+    </>
+  )
+}
+
+export default App
