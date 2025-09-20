5cf53a8299fd57379736fed403c29214d6c51f0a
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-20 18:54:16 +0600

    updated HomePage layouts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import Hero from '../components/hero'
-import SuccessCards from '../components/SuccessCards';
-import LawyerDetails from './LawyerDetails';
+import React from "react";
+import Hero from "../components/hero";
+import SuccessCards from "../components/SuccessCards";
+import LawyerDetails from "./LawyerDetails";
 
 const HomePage = () => {
   return (
-    <div className='container mx-auto ' >
-      <Hero/>
-
-      <div>
-      <LawyerDetails/>
-      </div>
-      
-      <SuccessCards/>
+    <div className="container mx-auto ">
+      <Hero />
+      <LawyerDetails />
+      <SuccessCards />
     </div>
   );
 };
